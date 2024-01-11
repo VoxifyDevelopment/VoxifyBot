51a58ea6370f2ccb46459cd0a11c21c464bbbb00
sanguine6660 <sanguine6660@gmail.com>
2024-01-11 17:59:08 +0100

    Update premium load

--- src/client/PremiumFeatures.ts
+++ src/client/PremiumFeatures.ts
@@ -1,14 +1,14 @@
 import * as fs from 'fs';
 import VoxifyClient from './VoxifyClient';
 
-const premiumFolder = './premium';
-const premiumIndex =
-    process.env.NODE_ENV === 'production' ? './premium/index.js' : './premium/index.ts';
+const premiumFolder = __dirname + '/premium';
 
 export default async function premiumCheckAndAdd(bot: VoxifyClient) {
-    if (fs.existsSync(premiumFolder) && fs.existsSync(premiumIndex)) {
-        const premium = await import(premiumIndex).catch(console.error);
+    if (fs.existsSync(premiumFolder)) {
+        bot.out.debug('Found premium folder...');
+        const premium = await import(premiumFolder).catch(console.error);
         if (premium.default) {
+            bot.out.debug('Found premium index...');
             premium.default(bot);
             return true;
         }
