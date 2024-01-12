39b46ffe8c5fc938044cad89e0dc5dfb2fef22c3
sanguine6660 <sanguine6660@gmail.com>
2024-01-12 02:43:55 +0100

    Add default button behavior

---
+++ src/client/interactions/buttonCommands/control-status.ts
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2023 - present | sanguine6660 <sanguine6660@gmail.com>
+ * Copyright (c) 2023 - present | voxify.dev <contact@voxify.dev>
+ * Copyright (c) 2023 - present | voxify.dev team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { PermissionFlagsBits } from 'discord.js';
+import ButtonCommandExecutor, { ButtonCommandEvent } from '../../executors/ButtonCommandExecutor';
+
+export default class TeamList implements ButtonCommandExecutor {
+    async execute(event: ButtonCommandEvent) {
+        const { bot, interaction } = event;
+        if (!interaction.guild) return false;
+
+        let guildLocaleName = interaction.guild?.preferredLocale.toLowerCase() || 'en-us';
+        let localeName = bot.translations.translations[interaction.locale.toLowerCase()]
+            ? interaction.locale.toLowerCase()
+            : guildLocaleName;
+
+        const resolvedArgs = await bot.tools.discord
+            .resolveTempVoiceArgs(bot, interaction)
+            .catch(console.error);
+        if (!resolvedArgs || resolvedArgs === true) {
+            bot.out.debug('OUT: resolvedArgs not existent');
+            return false;
+        }
+
+        if (
+            !(await bot.tools.discord.checkTvcArgs(
+                localeName,
+                resolvedArgs,
+                bot.translations.translateTo(localeName, 'buttons.close.name'),
+                bot,
+                interaction,
+                true,
+                true,
+                PermissionFlagsBits.ManageChannels
+            ))
+        )
+            return false;
+
+        const { member, channel } = resolvedArgs;
+
+        await interaction
+            .reply({
+                ephemeral: true,
+                content: 'not implemented yet'
+            })
+            .catch(console.error);
+
+        return true;
+    }
+}
